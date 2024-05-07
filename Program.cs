cb4d452701b4d308921e78de6f28c7e93da51682
dotvac <169125702+dotvac@users.noreply.github.com>
2024-05-07 09:12:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+// Base class for musical instruments
+class Instrument
+{
+    public string Name { get; set; }
+    public string Type { get; set; }
+
+    public void Play()
+    {
+        Console.WriteLine($"Playing {Name}");
+    }
+}
+
+// Derived class for string instruments
+class StringInstrument : Instrument
+{
+    public int NumStrings { get; set; }
+
+    public StringInstrument(string name, int numStrings)
+    {
+        Name = name;
+        NumStrings = numStrings;
+        Type = "String";
+    }
+
+    public void Pluck()
+    {
+        Console.WriteLine($"Plucking the {Name}");
+    }
+}
+
+// Derived class for wind instruments
+class WindInstrument : Instrument
+{
+    public string Material { get; set; }
+
+    public WindInstrument(string name, string material)
+    {
+        Name = name;
+        Material = material;
+        Type = "Wind";
+    }
+
+    public void Blow()
+    {
+        Console.WriteLine($"Blowing into the {Name}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        StringInstrument guitar = new StringInstrument("Guitar", 6);
+        guitar.Play();
+        guitar.Pluck();
+
+        WindInstrument flute = new WindInstrument("Flute", "Wood");
+        flute.Play();
+        flute.Blow();
+    }
+}
