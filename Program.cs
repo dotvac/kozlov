ed2ee5805174945ae5be6a355fcc1f240a5e269d
dotvac <169125702+dotvac@users.noreply.github.com>
2024-05-14 09:31:22 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,16 +5,19 @@ class Program
 {
     static void Main()
     {
-        Guitar guitar = new Guitar("Classical Guitar", "Wood", 2018);
-        Piano piano = new Piano("Grand Piano", "Wood", 2016);
+        Guitar guitar = new Guitar("Classical Guitar", "Wood", 2018, "Nylon");
+        Piano piano = new Piano("Grand Piano", "Wood", 2016, 88);
 
         guitar.DisplayInfo();
         guitar.Play();
         guitar.Tune();
+        Console.WriteLine($"String Type: {guitar.StringType}");
 
         Console.WriteLine();
 
         piano.DisplayInfo();
         piano.Play();
         piano.Tune();
+        Console.WriteLine($"Number of Keys: {piano.NumberOfKeys}");
     }
+}
